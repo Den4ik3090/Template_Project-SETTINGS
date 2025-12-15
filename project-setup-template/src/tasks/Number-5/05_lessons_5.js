f47fbb9db1e2a96ca4b26a0f2d2872cce637edb3
Den4ik3090 <grishaevds@yandex.ru>
2025-12-15 15:28:52 +0300

    complete_footer

--- project-setup-template/src/tasks/Number-5/05_lessons_5.js
+++ project-setup-template/src/tasks/Number-5/05_lessons_5.js
@@ -13,6 +13,12 @@ import coffeeBeanBg from '../../assets/images/lesson_5/coffee_bean.png';
 import cupBg from '../../assets/images/lesson_5/cup.png';
 import avatarMan from '../../assets/images/lesson_5/avatar/avatar_man.png';
 import avatarWoman from '../../assets/images/lesson_5/avatar/avatar_wooman.png';
+import blast from '../../assets/images/lesson_5/coffee_blast.png';
+import iconf from '../../assets/images/lesson_5/icons/Facabook.png';
+import iconInst from '../../assets/images/lesson_5/icons/Insta.png';
+import iconYT from '../../assets/images/lesson_5/icons/YouTube.png';
+import iconTwitter from '../../assets/images/lesson_5/icons/Twitter.png';
+import cupTea from '../../assets/images/lesson_5/cup-tea.png';
 
 const app = document.querySelector('#app');
 app.innerHTML = '';
@@ -63,8 +69,8 @@ app.innerHTML = `
             <h1 class="hero__title display-4 fw-bold mb-5 mt-5">Coffee</h1>
             <p class="hero__description mb-4 col-md-10 mt-2">Краткое описание, пару строк текста про сервис или сайт, почему этот кофе особенный.</p>
             <div class="hero__actions d-flex justify-content-center justify-content-md-start gap-3">
-              <a href="#menu" class="btn btn-warning hero__btn hero__btn--primary">ЗАКАЗАТЬ</a>
-              <a href="#menu" class="btn btn-danger btn-outline-light hero__btn hero__btn--secondary">МЕНЮ</a>
+              <a href="#menu" class="btn subscribe__btn-input btn-warning rounded-pill  py-3 shadow-sm btn-hover-effect">ЗАКАЗАТЬ</a>
+              <a href="#menu" class="btn subscribe__btn-input btn-danger btn-outline-light py-3 btn_menu">МЕНЮ</a>
             </div>
           </div>
           <div class="col-12 col-md-6 hero__media"></div>
@@ -74,13 +80,16 @@ app.innerHTML = `
       <!-- DISCOVER SECTION -->
       <section class="discover container-fluid">
         <figure>
-          <img src="${coffeeBlastImg}" class="img_blast d-md-none" alt="" />
+          <img src="${coffeeBlastImg}" class="img_blast-discover " alt="" />
+          <img src="${coffeeBlastImg}" class="img_blast-discoverRight d-md-none" alt="" />
         </figure>
         <div class="row align-items-center">
           <div class="col-12 col-md-6 discover__content">
             <h2 class="discover__title">Discover the best coffee</h2>
-            <p class="discover__text">Наше кафе «Птички» варит по‑настоящему хороший кофе...</p>
-            <button type="button" class="btn btn-warning discover__btn">Подробнее</button>
+            <p class="discover__text">Секрет нашего кофе прост и сложен одновременно. Мы отбираем только 100% арабику класса Specialty с лучших ферм Эфиопии, Бразилии и Колумбии. Каждая партия проходит авторскую обжарку, чтобы сохранить уникальные ноты шоколада, цитрусов или ягод.
+
+Для нас «лучший кофе» — это не просто слова. Это утренний ритуал, который задает ритм вашему дню.</p>
+            <button type="button" class="btn discover__btn  btn-warning rounded-pill  py-2 shadow-sm btn-hover-effect ">Подробнее</button>
           </div>
           <div class="col-12 col-md-6 text-end discover__media position-relative">
             <img src="${cofeeImg}" class="discover__img img-fluid" alt="Cup of coffee" loading="lazy" />
@@ -122,7 +131,7 @@ app.innerHTML = `
                 </div></article>
               </div>
               <div class="swiper-slide">
-                 <article class="h-100"><div class="card h-100 border-0 shadow-sm product-card">
+                  <article class="h-100"><div class="card h-100 border-0 shadow-sm product-card">
                   <div class="product-card__img-wrapper card-img-top-wrapper"><img src="${menuLatte}" class="card-img-top" alt="Matcha" loading="lazy" /></div>
                   <div class="card-body d-flex flex-column"><h3 class="card-title fw-bold">Матча</h3><p class="card-text text-muted small">Кофе 50% | Молоко 50%</p><div class="mt-auto text-center"><span class="fw-bold d-block">200руб</span><button type="button" class="btn btn-warning">Order Now</button></div></div>
                 </div></article>
@@ -137,21 +146,21 @@ app.innerHTML = `
       <section class="about" id="about">
         <div class="about__text-block"><h1 class="text-block__title">Чем мы отличаемся?</h1><p class="text-block__text">Мы варим не просто кофе...</p></div>
         <div class="about__card row container-fluid py-4">
-          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconBadge}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Свежие зерна</h2><p class="block_text">Кофе с таким вкусом...</p></div></div>
-          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconBeans}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Высокое качество</h2><p class="block_text">Coffee like you...</p></div></div>
-          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconCup}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Большой выбор</h2><p class="block_text">Coffee like you...</p></div></div>
-          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconPrice}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Лучшие цены</h2><p class="block_text">Coffee like you...</p></div></div>
-          <div class="text-block__bottom"><p class="text-block__title-bottom m-0">Great ideas start with great coffee...</p><h2 class="text-block__h2-bottom m-0">Get started today.</h2></div>
-          <button class="btn btn-warning text-block__bottom-btn">Join Us</button>
+          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconBadge}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Свежие зерна</h2><p class="block_text">Свежесть кофе которую ты точно почувствуешь</p></div></div>
+          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconBeans}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Высокое качество</h2><p class="block_text">Уделяем внимание каждой мелочи </p></div></div>
+          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconCup}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Большой выбор</h2><p class="block_text">Кофе под любое настроение.</p></div></div>
+          <div class="card-item col-12 col-md-3 p-md-0"><div class="card-item__img"><img src="${iconPrice}" alt="" /></div><div class="card-item__block"><h2 class="block_title">Лучшие цены</h2><p class="block_text">Удивляем не только вкусом ,но и ценой!</p></div></div>
+          <div class="text-block__bottom"><p class="text-block__title-bottom m-0">Отличные идеи начинаются с отличного кофе</p><h2 class="text-block__h2-bottom m-0">Начни уже сегодня </h2></div>
+          <button class="btn  btn-warning rounded-pill px-5 py-2 shadow-sm btn-hover-effect text-block__bottom-btn ">Будь частью нашей команды !</button>
         </div>
       </section>
 
       <section class="breakfast">
         <div class="container"><div class="row justify-content-center justify-content-lg-start">
           <div class="breakfast__text-block col-12 col-sm-10 col-md-8 col-lg-6 pt-3 pt-md-5 text-center text-lg-start">
-            <h1 class="breakfast__title display-5 fw-bold mb-3">Get a chance to have an Amazing morning</h1>
-            <p class="breakfast__text lead mb-4">We are giving you are one time opportunity...</p>
-            <button class="breakfast__btn btn btn-warning w-md-auto">Order Now</button>
+            <h1 class="breakfast__title display-5 fw-bold mb-3">Воспользуйся шансом провести свое лучшее утро </h1>
+            <p class="breakfast__text lead mb-4">С чашечкой нашего кофе...</p>
+            <button class="btn subscribe__btn-input btn-warning rounded-pill px-5 py-2 shadow-sm btn-hover-effect">Order Now</button>
           </div>
         </div></div>
         <figure><img src="${coffeeBeanBg}" alt="Coffee" class="breakfast__bg-image" /></figure>
@@ -161,14 +170,14 @@ app.innerHTML = `
 
     <!-- Section FEEDBACK -->
     <section class="feedback">
-      <h1 class="feedback__title">Our coffee perfection feedback</h1>
-      <p class="feedback__text">Our customers has amazing things to say about us</p>
+      <h1 class="feedback__title">Отзывы о нашем идеальном кофе</h1>
+      <p class="feedback__text">За наше качество , говорят наши клиенты </p>
       <div class="feedback__slider">
         <button class="feedback__btn-left btn btn-warning" aria-label="Предыдущий отзыв">←</button>
         <button class="feedback__btn-rigth btn btn-warning" aria-label="Следующий отзыв">→</button>
         <div class="feedback__blocks-container">
           <div class="feedback__block feedback__block--active">
-            <p class="feedback__block-text">Отлличное заведение , крутой ремонт, прекрасное обслуживание и кончено же классный коллектив. Ах да , кофе выше всех пекрас. Спасибо что стараетесь для нас !</p>
+            <p class="feedback__block-text">Отличное заведение , крутой ремонт, прекрасное обслуживание и кончено же классный коллектив. Ах да , кофе выше всех пекрас. Спасибо что стараетесь для нас !</p>
             <h2 class="feedback__block-name">Grishaev Denis</h2>
             <p class="feedback__block-prof">Project Manager</p>
             <img src="${avatarMan}" alt="Grishaev Denis" class="feedback__block-photo" />
@@ -185,14 +194,94 @@ app.innerHTML = `
             <p class="feedback__block-prof">Дизайнер</p>
             <img src="" alt="Михаил Сидоров" class="feedback__block-photo" />
           </div>
+           
         </div>
       </div>
+               <img src="${blast}" alt="" class="img-blast-left " />
+                   <img src="${blast}" alt="" class="img-blast-right" />
+
     </section>
 
-    <!-- Footer -->
-    <footer class="footer py-3 text-center">
-      <p class="small text-muted mb-0">&copy; 2025 Denis Grishaev. All rights reserved.</p>
-    </footer>
+<!-- Subscribe Section -->
+<section class="subscribe-section">
+  <div class="subscribe__block">
+    <h1 class="subscribe__title">Подпишитесь, чтобы получать последние новости</h1>
+    <p class="subscribe__text">Не пропустите наши последние новости, обновления, советы и специальные предложения</p>
+    
+    <!-- ВАЖНО: Контейнер-обертка для формы -->
+    <div class="subscribe__form-container">
+      
+      <!-- 1. Поле ввода (Bootstrap Floating Label) -->
+      <div class="form-floating text-dark subscribe__input-wrapper">
+        <input type="email" class="form-control subscribe__input" id="subscribeEmail" placeholder="name@example.com">
+        <label for="subscribeEmail">Введите адрес электронной почты</label>
+      </div>
+
+      <!-- 2. Кнопка (Отдельно от инпута) -->
+      <button class="btn subscribe__btn-input btn-warning rounded-pill px-5 py-3 shadow-sm btn-hover-effect">
+        Подписаться
+      </button>
+
+    </div>
+  </div>
+</section>
+
+<!-- Footer -->
+<footer>
+  <div class="footer">
+    <!-- Декор (скроем на мобильных) -->
+    <img src="${cupTea}" class="footer-img__cup-left" alt="cup-tea">
+    <img src="${cupTea}" class="footer-img__cup-right" alt="cup-tea">
+
+    <!-- Левый блок (Logo + Social) -->
+    <div class="footer__block-left">
+      <h2 class="block-left__title">
+        <a href="/" class="navbar__logo-link">
+          <img src="${logoImg}" class="navbar__logo-img" alt="Bean Scene Logo" />
+        </a>
+      </h2>
+      <p class="block-left__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      <div class="block-left_icon">
+        <img src="${iconf}" class="footer_icon" alt="facebook" />
+        <img src="${iconTwitter}" class="footer_icon" alt="twitter" />
+        <img src="${iconInst}" class="footer_icon" alt="instagramm" />
+        <img src="${iconYT}" class="footer_icon" alt="youtube" />
+      </div>
+    </div>
+
+    <!-- Правый блок (Ссылки) -->
+    <div class="footer__block-right">
+      <div class="block-right__column">
+        <h2>About</h2>
+        <ul class="footer__list">
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Support</a></li>
+        </ul>
+      </div>
+      <div class="block-right__column">
+        <h2>Company</h2>
+        <ul class="footer__list">
+          <li><a href="#">How we work</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Support</a></li>
+        </ul>
+      </div>
+      <div class="block-right__column">
+        <h2>Contact Us</h2>
+        <ul class="footer__list">
+          <li><a href="#">Address</a></li>
+          <li><a href="#">+1 345 345 34</a></li>
+          <li><a href="#">news@coffee.com</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+   
   `;
 
 const burgerBtn = document.getElementById('burgerBtn');
@@ -283,7 +372,7 @@ class FeedbackSlider {
   autoSlide() {
     setInterval(() => {
       this.next();
-    }, 10000);
+    }, 100000);
   }
 }
 
